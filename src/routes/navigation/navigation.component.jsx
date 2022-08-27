95fd4688e1a7cf2acd3a5462cb33059e97f008a6
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-27 18:53:31 +0100

    import Fragment

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,14 @@
+import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Navigation = () => {
     return (
-      <div>
-        <div>
+      <Fragment>
+        <div className='navigation'>
           <h1>I am the navigation bar</h1>
         </div>
         <Outlet />
-      </div>
+      </Fragment>
     );
 }
 
