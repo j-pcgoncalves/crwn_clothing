562aff44556a8ba1f485ed53cd95c26c80800945
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-27 19:54:34 +0100

    import signInWithGooglePopup in SignIn

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,16 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+    }
+    
     return (
         <div>
             <h1>SIGN IN PAGE</h1>
+            <button onClick={logGoogleUser}>
+                Sign In
+            </button>
         </div>
     )
 }
