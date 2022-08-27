d842cd8ff0c8f7f8f6bcdb0fe523250736983cc6
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-27 19:03:36 +0100

    import styles to Navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,8 @@ import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo }  from '../../assets/crown.svg';
 
+import './navigation.styles.scss';
+
 const Navigation = () => {
     return (
         <Fragment>
