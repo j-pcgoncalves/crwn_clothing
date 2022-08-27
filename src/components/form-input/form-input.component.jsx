e78ad4f07765f8c89e50bb589f726387b05165e3
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-27 22:21:17 +0100

    shrinking label when in focus

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -3,13 +3,13 @@ import './form-input.styles.scss';
 const FormInput = ({ label, ...otherProps  }) => {
     return (
         <div className="group">
+            <input className="form-input" {...otherProps} />
             {label && (
                 <label 
                     className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
                     {label}
                 </label>
             )}
-            <input className="form-input" {...otherProps} />
         </div>
     )
 }
