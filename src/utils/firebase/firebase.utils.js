e2813e68ec8d5068ae093f5da48e364522ef3e34
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-27 21:43:53 +0100

    create functionality to auth email and pass

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,7 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { 
     getAuth, 
     signInWithPopup,  
-    GoogleAuthProvider
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth';
 import {
     getFirestore,
@@ -33,6 +34,8 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
+    if (!userAuth) return;
+
     const userDocRef = doc(db, 'users', userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
 
@@ -47,9 +50,15 @@ export const createUserDocumentFromAuth = async (userAuth) => {
                 createdAt
             });
         } catch (error) {
-            console.log('Error creating the user', error.message);
+            console.log('Error creating the user!', error.message);
         }
     }
 
     return userDocRef;
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+    
+    return await createUserWithEmailAndPassword(auth, email, password);
 }
