5f7509e165ab85314132eadf34e7801c33e36bb5
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-27 22:38:10 +0100

    adding styles to submit button

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 import './sign-up-form.styles.scss';
@@ -85,7 +86,7 @@ const SignUpForm = () => {
                     value={confirmPassword} 
                 />
 
-                <button type='submit'>Sign Up</button> 
+                <Button type='submit'>Sign Up</Button> 
             </form>
         </div>
     )
