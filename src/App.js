6d9f5f1d445298bbf42cf15a231bc85519dac55b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-27 17:35:57 +0100

    adding images as backgroundImage

--- src/App.js
+++ src/App.js
@@ -31,9 +31,14 @@ const App = () => {
   
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => (
+      {categories.map(({ title, id, imageUrl }) => (
         <div key={id} className="category-container">
-          <div className="background-image" />
+          <div 
+            className="background-image" 
+            style={{
+              backgroundImage: `url(${imageUrl})`
+            }} 
+          />
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
