46cc6fc5b1fcf22d10639251c8a38d9d1c133592
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-27 20:47:17 +0100

    delete signInWithGoogleRedirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,19 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+    
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
-    
+
     return (
         <div>
             <h1>SIGN IN PAGE</h1>
-            <button onClick={logGoogleUser}>
-                Sign In
-            </button>
+            <button onClick={logGoogleUser}>Sign In with Google Popup</button>
         </div>
     )
 }
