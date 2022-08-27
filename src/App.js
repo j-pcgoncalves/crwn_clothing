b8c8854cfd2aa6dd40cdc1fb793e54772a1f8d06
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-27 17:44:46 +0100

    create CategoryItem component

--- src/App.js
+++ src/App.js
@@ -32,18 +32,7 @@ const App = () => {
   return (
     <div className="categories-container">
       {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div 
-            className="background-image" 
-            style={{
-              backgroundImage: `url(${imageUrl})`
-            }} 
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+        
       ))}
     </div>
   );
