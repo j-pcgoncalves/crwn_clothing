c45e4aca7d0c861a67a8f0b1f0afc3644822ff51
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-28 21:41:39 +0100

    create removeCartItem()

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -13,11 +13,26 @@ const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1 }];
 }
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+    );
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    removeItemFromCart: () => {},
     cartCount: 0
 });
 
@@ -32,8 +47,12 @@ export const CartProvider = ({ children }) => {
 
     }, [cartItems]);
 
-    const addItemToCart = productToAdd=> {
+    const addItemToCart = productToAdd => {
         setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const removeItemFromCart = cartItemToRemove => {
+        setCartItems(addCartItem(cartItems, cartItemToRemove));
     }
 
     const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
