3cbc87e4ac3afdc00996316c4b87a71924ea76c8
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-28 19:18:28 +0100

    import ProductsContext to Shop

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
