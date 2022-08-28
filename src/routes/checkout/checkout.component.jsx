87b74154db2478cef37215634f40e13581fc689d
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-28 21:51:25 +0100

    add styles to checkout component

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -8,26 +8,41 @@ const Checkout = () => {
     const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
     
     return (
-        <div>
-            <h1>I am the checkout page</h1>
-            <div>
-                {
-                    cartItems.map(cartItem => {
-                        const { id, name, quantity } = cartItem;
-                        
-                        return (
-                            <div key={id}>
-                                <h2>{name}</h2>
-                                <span>{quantity}</span>
-                                <br />
-                                <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-                                <br />
-                                <span onClick={() => addItemToCart(cartItem)}>increment</span>
-                            </div>
-                        );
-                    })
-                }
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>    
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>   
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>  
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>   
+                </div>
             </div>
+            {
+                cartItems.map(cartItem => {
+                    const { id, name, quantity } = cartItem;
+                    
+                    return (
+                        <div key={id}>
+                            <h2>{name}</h2>
+                            <span>{quantity}</span>
+                            <br />
+                            <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
+                            <br />
+                            <span onClick={() => addItemToCart(cartItem)}>increment</span>
+                        </div>
+                    );
+                })
+            }
+            <span className='total'>Total: 0</span>
         </div>
     )
 }
