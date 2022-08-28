f2fe765916ba4a847d5f848cd2e6720d6c692efe
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-28 22:03:56 +0100

    create clearCartItem()

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -27,12 +27,17 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     );
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
     cartCount: 0
 });
 
@@ -53,6 +58,10 @@ export const CartProvider = ({ children }) => {
 
     const removeItemFromCart = cartItemToRemove => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove));
+    }
+
+    const clearItemFromCart = cartItemToClear => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear));
     }
 
     const value = { 
