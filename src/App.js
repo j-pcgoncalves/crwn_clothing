c2a5145f015f00b3410f2a4a2cf16a7699a4593f
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-01-04 23:00:10 +0000

    create Directory component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import "./categories.styles.scss";
-
-import CategoryItem from "./components/category-item/category-item.component";
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
   const categories = [
     {
-      "id": 1,
-      "title": "Hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
-      "id": 2,
-      "title": "Jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
-      "id": 3,
-      "title": "Sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
-      "id": 4,
-      "title": "Women's",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+      id: 4,
+      title: 'Women\'s',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
-      "id": 5,
-      "title": "Men's",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
+      id: 5,
+      title: 'Men\'s',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
   ];
 
-  return (
-    <div className="categories-container">
-      { categories.map(category => (
-        <CategoryItem key={category.id} category={category} />
-      )) }
-    </div>
-  );
+  return <Directory categories={categories} />;
 }
 
 export default App;
