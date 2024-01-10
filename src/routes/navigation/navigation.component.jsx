25d4182f2f58ab466d3f36c52850c6172006a91b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-01-10 22:32:48 +0000

    create CartIcon and CartDropdown component and add styles

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,9 @@ import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.scss";
@@ -21,6 +24,7 @@ const Navigation = () => {
                     <Link className="nav-link" to="/shop">
                         SHOP
                     </Link>
+
                     {
                         currentUser ? (
                             <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
@@ -30,7 +34,12 @@ const Navigation = () => {
                             </Link>
                         )
                     }
+
+                    <CartIcon />
                 </div>
+
+                <CartDropdown />
+                
             </div>
             <Outlet />
         </Fragment>
