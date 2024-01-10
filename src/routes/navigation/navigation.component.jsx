d784c338953937d4567739b06b572e5f2cee81e6
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-01-10 22:41:54 +0000

    create cart context and toggle dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
@@ -13,6 +14,7 @@ import "./navigation.styles.scss";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -37,8 +39,10 @@ const Navigation = () => {
 
                     <CartIcon />
                 </div>
-
-                <CartDropdown />
+                
+                {
+                    isCartOpen && <CartDropdown /> 
+                }
                 
             </div>
             <Outlet />
