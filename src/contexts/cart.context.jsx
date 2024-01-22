dba20fcef09e5f0ad595c5f451e275fb73fa448c
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-01-22 21:47:46 +0000

    create reducer utils to createAction

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,7 @@
 import { createContext, useReducer } from "react";
 
+import { createAction } from "../utils/reducer/reducer.utils";
+
 const addCartItem = (cartItems, productToAdd) => {
     const existingCartItem = cartItems.find(
         (cartItem) => cartItem.id === productToAdd.id
@@ -85,14 +87,13 @@ export const CartProvider = ({ children }) => {
         const newCartCount = newCartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
         const newCartTotal = newCartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
 
-        dispatch({ 
-            type: CART_ACTION_TYPES.SET_CART_ITEMS, 
-            payload: { 
+        dispatch(
+            createAction(CART_ACTION_TYPES.SET_CART_ITEMS, { 
                 cartItems: newCartItems, 
                 cartTotal: newCartTotal, 
                 cartCount: newCartCount 
-            } 
-        });
+            })
+        );
     }
 
     const addItemToCart = (productToAdd) => {
@@ -111,10 +112,9 @@ export const CartProvider = ({ children }) => {
     }
 
     const setIsCartOpen = (bool) => {
-        dispatch({ 
-            type: CART_ACTION_TYPES.SET_IS_CART_OPEN, 
-            payload: bool 
-        });
+        dispatch(
+            createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool)  
+        );
     }
 
     const value = { 
